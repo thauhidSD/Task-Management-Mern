b2783dd48e0abe9e03669f6abaf4c3f349208fc7
Md Thauhid Hossain <thethauhid103@gmail.com>
2026-04-14 19:51:25 +0600

    Middleta to handle cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
+
+// Middleware to handle cors
+app.use(
+    cors({
+    origin: process.env.FRONT_END_URL || "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", DELETE],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    })
+)
+
+// Middleware to handle json oject in request body
+app.use(express.json())
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000!")
