c6b2ef78e7d6f7e0af687f26aa840b4eab0c92b0
Md Thauhid Hossain <thethauhid103@gmail.com>
2026-04-24 10:19:25 +0600

    update user profile

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { signin, signup, userProfile } from "../controller/auth.controller.js"
+import { signin, signup, updateUserProfile, userProfile } from "../controller/auth.controller.js"
 import { verifyToken } from "../utils/verifyUser.js"
 
 const router = express.Router()
@@ -9,5 +9,7 @@ router.post("/sign-up", signup)
 router.post("/sign-in", signin)
 
 router.get("/user-profile", verifyToken, userProfile)
+
+router.put("/update-profile", verifyToken, updateUserProfile)
 
 export default router
