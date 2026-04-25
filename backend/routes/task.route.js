bdfc2d4330964d6dcaec283ba8a795d5c725c551
Md Thauhid Hossain <thethauhid103@gmail.com>
2026-04-25 13:48:11 +0600

    get task api route

--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
-import { createTask } from "../controller/task.controller.js"
+import { createTask, getTasks } from "../controller/task.controller.js"
 
 const router = express.Router()
 
 router.post("/create", verifyToken, adminOnly, createTask)
+
+router.get("/", verifyToken, getTasks)
 
 export default router
