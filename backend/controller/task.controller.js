64489a9387914fafb292253eadbbf138ef72cf6a
Md Thauhid Hossain <thethauhid103@gmail.com>
2026-04-28 15:57:14 +0600

    Update task api route

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -120,3 +120,36 @@ export const getTaskById = async(req, res, next) => {
         next(error)
     }
 }
+
+export const updateTask = async(req, res, next) => {
+    try {
+        const task = await Task.findById(req.params.id)
+
+        if(!task) {
+            return next(errorHandler(404, "Task Not Found!"))
+        }
+
+        task.title = req.body.title || task.title
+        task.description = req.body.description || task.description
+        task.priority = req.body.priority || task.priority
+        task.dueDate = req.body.dueDate || task.dueDate
+        task.todoChecklist = req.body.todoChecklist || task.todoChecklist
+        task.attachments = req.body.attachments || task.attachments
+
+        if(req.body.assignedTo){
+            if(!Array.isArray(req.body.assignedTo)){
+                return next(
+                    errorHandler(400, "assignedTo must be an array of user IDs")
+                )
+            }
+
+            task.assignedTo = req.body.assignedTo
+        }
+
+        const updateTask = await task.save()
+
+        return res.status(200).json({ updateTask, message: "Task updated successfully!"})
+    } catch (error) {
+        next(error)
+    }
+}
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { adminOnly, verifyToken } from "../utils/verifyUser.js"
-import { createTask, getTaskById, getTasks } from "../controller/task.controller.js"
+import { createTask, getTaskById, getTasks, updateTask } from "../controller/task.controller.js"
 
 const router = express.Router()
 
@@ -9,5 +9,7 @@ router.post("/create", verifyToken, adminOnly, createTask)
 router.get("/", verifyToken, getTasks)
 
 router.get("/:id", verifyToken, getTaskById)
+
+router.put("/:id", verifyToken, updateTask)
 
 export default router
